ea5bb20e72f498e9f3c95f1f99391799a0ee4edb
wahyufahrozi <wahyufahrozi97@gmail.com>
2019-12-16 19:04:35 +0700

    Update inbox

--- src/Screens/Inbox.js
+++ src/Screens/Inbox.js
@@ -6,14 +6,61 @@ import {
   View,
   Text,
   StatusBar,
+  Image,
 } from 'react-native';
+import {Left} from 'native-base';
 
 export default class Inbox extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Ini Halaman Inbox</Text>
-      </View>
+      <>
+        <View style={styles.contentInbox}>
+          <View style={styles.headerInbox}>
+            <Text
+              style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+              }}>
+              Inbox
+            </Text>
+            <Text style={{color: 'grey'}}>
+              Messages from your host will appear here.
+            </Text>
+          </View>
+          <View
+            style={
+              {
+                // justifyContent: 'center',
+                // alignContent: 'center',
+              }
+            }>
+            <Image
+              source={require('../Assets/inbox.jpg')}
+              style={{
+                width: 300,
+                height: 300,
+                alignSelf: 'center',
+                position: 'absolute',
+                top: 180,
+              }}
+            />
+          </View>
+        </View>
+      </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  contentInbox: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerInbox: {
+    flexDirection: 'column',
+    position: 'absolute',
+    left: 20,
+    top: 20,
+    backgroundColor: 'white',
+  },
+});
