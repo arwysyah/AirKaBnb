f0953593bdffa9a265e41473715966fae0631f04
wahyufahrozi <wahyufahrozi97@gmail.com>
2019-12-16 19:13:00 +0700

    update saved

--- src/Screens/Saved.js
+++ src/Screens/Saved.js
@@ -6,14 +6,62 @@ import {
   View,
   Text,
   StatusBar,
+  Image,
 } from 'react-native';
+import {Left} from 'native-base';
 
 export default class Saved extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Ini Halaman Saved</Text>
-      </View>
+      <>
+        <View style={styles.contentSaved}>
+          <View style={styles.headerSaved}>
+            <Text
+              style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                marginBottom: 30,
+              }}>
+              Saved
+            </Text>
+            <Text style={{color: 'grey', width: 300}}>
+              Collect places to stay and things to do by tapping the heart icon.
+            </Text>
+          </View>
+          <View
+            style={
+              {
+                // justifyContent: 'center',
+                // alignContent: 'center',
+              }
+            }>
+            <Image
+              source={require('../Assets/saved.jpg')}
+              style={{
+                width: 300,
+                height: 300,
+                alignSelf: 'center',
+                position: 'absolute',
+                top: 180,
+              }}
+            />
+          </View>
+        </View>
+      </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  contentSaved: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerSaved: {
+    flexDirection: 'column',
+    position: 'absolute',
+    left: 20,
+    top: 30,
+    backgroundColor: 'white',
+  },
+});
